2c455c80006fdae7ca429801f94c0c3df8344151
unknown <ymlee75@cp51508.cplanet.co.kr>
2013-11-15 18:34:01 +0900

    no message

--- discard/src/DiscardServerHandler.java
+++ discard/src/DiscardServerHandler.java
@@ -1,6 +1,8 @@
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.buffer.ByteBuf;
+import io.netty.util.ReferenceCountUtil;
+
 
 /**
  * Handles a server-side channel.
@@ -9,8 +11,8 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        // Discard the received data silently.
-        ((ByteBuf) msg).release(); // (3)
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
     }
 
     @Override
