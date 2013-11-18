112fc4daa49e22339af2df44a4cb6fbe518d58b3
unknown <ymlee75@cp51508.cplanet.co.kr>
2013-11-18 10:02:57 +0900

    netty using object serialize

--- discard/src/TimeClientHandler.java
+++ discard/src/TimeClientHandler.java
@@ -3,19 +3,16 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.util.Date;
+import java.util.List;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf m = (ByteBuf) msg; // (1)
-        try {
-            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
-            System.out.println(new Date(currentTimeMillis));
-            ctx.close();
-        } finally {
-            m.release();
-        }
+        UnixTime m = (UnixTime) msg;
+        System.out.println(m);
+        ctx.close();
     }
+
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
